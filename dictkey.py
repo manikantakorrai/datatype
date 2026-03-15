ead1de48dbda0ae98a2839b6b3007c02a8663f75
manikantakorrai <manikanta.korrai2004@gmail.com>
2026-03-15 10:41:46 +0530

    Add files via upload

---
+++ dictkey.py
@@ -0,0 +1,9 @@
+employee = {
+    "id" : 97,
+    "name" : "manikanta"
+}
+employee["salary"] = 50000
+print("After adding salary :", employee)
+
+employee["salary"] = 60000
+print("After updating salary :", employee)
